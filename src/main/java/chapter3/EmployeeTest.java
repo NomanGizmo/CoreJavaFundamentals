2bbe5b84d521d2e35a7d2d94c13901df59159bbb
Noman Ansari <noman.ansari@halalbox.in>
2022-05-04 18:26:46 +0530

    chp3-handle null case

--- src/main/java/chapter3/EmployeeTest.java
+++ src/main/java/chapter3/EmployeeTest.java
@@ -1,6 +1,7 @@
 package chapter3;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class EmployeeTest {
     public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class EmployeeTest {
         Employee[] staff = new Employee[3];
         staff[0] = new Employee("Noman", 35000,2021,06,01);
         staff[1] = new Employee("Kakoli", 35000,2021,06,01);
-        staff[2] = new Employee("Swapnil", 35000,2021,06,01);
+        staff[2] = new Employee( null,35000,2021,06,01);
 //        staff[3] = new Employee("Anjali", 35000,2021,06,01);
 
         for (Employee e: staff) {
@@ -26,9 +27,11 @@ class Employee {
     private double salary;
     private LocalDate joiningDate;
 
-    Employee (String name, double salary, int year, int month, int day) {
-        this.name = name;
-        this.salary = salary;
+    Employee (String n, double s, int year, int month, int day) {
+//        this.name = Objects.requireNonNull(n,"the name cannot be null");
+        this.name = Objects.requireNonNullElse(n,"unknown");
+//        this.name = n;
+        this.salary = s;
         this.joiningDate = LocalDate.of(year,month,day);
     }
 
